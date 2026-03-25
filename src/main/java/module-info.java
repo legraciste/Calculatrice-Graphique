d9588f7d9ae484544d5aa8a776ea00bd9c1a4622
Tiecoura Mamadou Fofana <foft@info420.net>
2026-03-25 11:03:16 -0400

    Réalisation de la prémière partie du controller

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.calculatricegraphique {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens com.example.calculatricegraphique to javafx.fxml;
